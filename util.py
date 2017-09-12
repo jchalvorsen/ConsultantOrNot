812f42c52086fdabbb411fd827dcd891b708e0ea
Jon Christian Halvorsen <jchalvorsen@gmail.com>
2017-09-12 17:44:51 +0000

    refactor data processing

---
+++ util.py
@@ -0,0 +1,44 @@
+import numpy as np
+from numpy.random import random, permutation
+from scipy import misc, ndimage
+from scipy.ndimage.interpolation import zoom
+import matplotlib.pyplot as plt
+
+import keras
+from keras import backend as K
+from keras.utils.data_utils import get_file
+from keras.models import Sequential, Model
+from keras.layers.core import Flatten, Dense, Dropout, Lambda
+from keras.layers import Input
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.optimizers import SGD, RMSprop, Adam
+from keras.preprocessing import image
+
+
+# Mean of each channel as provided by VGG researchers
+vgg_mean = np.array([123.68, 116.779, 103.939]).reshape((3,1,1))
+
+def vgg_preprocess(x):
+    x = x - vgg_mean     # subtract mean
+    return x[:, ::-1]    # reverse axis bgr->rgb
+
+
+def get_batches(dirname, gen=image.ImageDataGenerator(), shuffle=True, 
+                batch_size=64, class_mode='categorical'):
+    return gen.flow_from_directory(dirname, target_size=(224,224), 
+                class_mode=class_mode, shuffle=shuffle, batch_size=batch_size)
+
+
+def plots(ims, figsize=(12,6), rows=1, interp=False, titles=None):
+    if type(ims[0]) is np.ndarray:
+        ims = np.array(ims).astype(np.uint8)
+        if (ims.shape[-1] != 3):
+            ims = ims.transpose((0,2,3,1))
+    f = plt.figure(figsize=figsize)
+    cols = len(ims)//rows if len(ims) % 2 == 0 else len(ims)//rows + 1
+    for i in range(len(ims)):
+        sp = f.add_subplot(rows, cols, i+1)
+        sp.axis('Off')
+        if titles is not None:
+            sp.set_title(titles[i], fontsize=16)
+        plt.imshow(ims[i], interpolation=None if interp else 'none')
