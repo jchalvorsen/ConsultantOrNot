883766e6fa747ac73aa6e3d6e1369be0529e4b7d
Jon Christian Halvorsen <jchalvorsen@gmail.com>
2017-10-17 21:05:52 +0000

    Add endpoint for using live webcam

--- webapp/hello.py
+++ webapp/hello.py
@@ -25,7 +25,6 @@ configure_uploads(app, photos)
 def upload():
     if request.method == 'POST' and 'photo' in request.files:
         filename = photos.save(request.files['photo'])
-        
         # Hent ut prediksjon
         img = misc.imread('./img/' + filename)
         img = misc.imresize(img, (224, 224))
@@ -33,11 +32,17 @@ def upload():
         img = np.expand_dims(img, axis=0).astype(np.uint8)
         preds, idxs, classes = predict(model, img)     
         return 'You are a ' + str(classes[0]) + ' with a confidence of ' + str(preds[0])
+    
     return render_template('upload.html')
+
+@app.route("/webcam")
+def webcam():
+    return render_template('webcam.html')
+
 
 @app.route("/")
 def hello():
-    return "Hello Nabla and Timini!"
+    return "Hello Westerdals!"
 
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', port=5000)
